22d501888cb234fe660be6aed346ccab82f0f30f
Yan Deak <yandeak@gmail.com>
2014-10-16 08:31:14 +0200

    patch
    " - " -> " x " ; jedna cela nova strana

--- Pyramida.java
+++ Pyramida.java
@@ -1,13 +1,18 @@
-/* Nakresli (krasnou) pyramidu. */
 public class Pyramida {
 	public static void main(String[] args) {
-		int vyska = Integer.parseInt(args[0]);
-		
-		for (int y = 0; y < vyska; y++) {
-			for (int x = 0; x < y * 2 + 1; x++) {
-				System.out.printf("-");
-			}
-			System.out.println();
-		}
+	       int vyska = Integer.parseInt(args[0]);
+
+	        for (int sloupec = 0; sloupec <= vyska; sloupec++) {
+
+	            for (int mezer =0; mezer <= vyska - sloupec; mezer++)
+	                System.out.print("  ");
+
+	            for (int predek = sloupec; predek >= 1; predek--)
+	                System.out.print(" x ");
+
+	            for (int bok = 1; bok <=sloupec; bok++)
+	                System.out.print("x");
+	            System.out.println();
+	        }  
+	    }
 	}
-}
